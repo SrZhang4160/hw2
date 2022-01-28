24df5a3c0dd1739a71a67edaeede71b52987eee3
Shuran Zhang <93342390+SrZhang4160@users.noreply.github.com>
2022-01-28 00:52:12 -0500

    Add files via upload

--- task-4/PaymentHistory.java
+++ task-4/PaymentHistory.java
@@ -1,6 +1,19 @@
 // Task 4
 
 public class PaymentHistory { 
+  List<Payment> paymenthistory;
+  User user;
+  
+  public PaymentHistory(User user){
+		this.user = user;
+  }
 
+  public Boolean add(Payment payment) { 
+	for(Payment p : paymenthistory){
+		if(p == payment) return false;
+	}   
+	paymenthistory.add(payment);
+	return true;
+	}          
 } 
 
